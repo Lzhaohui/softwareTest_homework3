004d71faa3955a79d7a650c032266817e0ab24a9
Li Zhaohui <137211885@qq.com>
2019-03-24 23:02:07 +0800

    第一次提交

---
+++ SoftwareTest/test/cn/tju/edu/st/TestPrintPrimes.java
@@ -0,0 +1,19 @@
+package cn.tju.edu.st;
+
+import org.junit.Test;
+
+public class TestPrintPrimes {
+
+	@Test
+	public void test1(){
+		PrintPrimes.printPrimes(1);
+	}
+	@Test
+	public void test3(){
+		PrintPrimes.printPrimes(3);
+	}
+	@Test
+	public void test5(){
+		PrintPrimes.printPrimes(5);
+	}
+}
